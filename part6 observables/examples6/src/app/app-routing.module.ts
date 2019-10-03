472b16e72151a58303564a4ebdec76e9aa21f34d
Kamil Wojcik <kwojcik@objectivity.co.uk>
2019-10-03 09:42:31 +0200

    part6 observables

---
+++ part6 observables/examples6/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { UserComponent } from './user/user.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'user/:id', component: UserComponent }
+];
+
+@NgModule({
+  imports: [
+//    RouterModule.forRoot(routes, { useHash: true })
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ part6 observables/examples6/src/app/user/user.component.ts
@@ -0,0 +1,28 @@
+import { UserService } from './../user.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+
+  id: number;
+
+  constructor(private route: ActivatedRoute, private userService: UserService) { }
+
+  ngOnInit() {
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.id = +params.id;
+      }
+    )
+  }
+
+  onActivate() {
+    this.userService.activatedEmitter.next(true);
+  }
+}
